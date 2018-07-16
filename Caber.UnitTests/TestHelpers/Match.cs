adb54cc42036377e20ffc2ec070374bc560a70f3
Alex Davidson <alex.davidson@bluewire-technologies.com>
2018-07-16 11:48:43 +0100

    Add helpers for testing configuration

---
+++ Caber.UnitTests/TestHelpers/Match.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+using Moq;
+
+namespace Caber.UnitTests.TestHelpers
+{
+    internal static class Match
+    {
+        /// <summary>
+        /// Generate an IEquatable which tests the specified expression.
+        /// </summary>
+        public static IEquatable<T> Object<T>(Expression<Func<T, bool>> match) => Mock.Of<IEquatable<T>>(e => e.Equals(It.Is(match)));
+    }
+}
---
+++ Caber.UnitTests/TestHelpers/TestDisposables.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Caber.UnitTests.TestHelpers
+{
+    public class TestDisposables
+    {
+        private static readonly ConcurrentDictionary<string, TestDisposables> DisposablesPerTest = new ConcurrentDictionary<string, TestDisposables>();
+
+        public static void Begin(string testId)
+        {
+            if (DisposablesPerTest.TryAdd(testId, new TestDisposables())) return;
+            throw new InvalidOperationException("Test context already has a list of disposables. BUG?");
+        }
+
+        public static TestDisposables For(string testId)
+        {
+            if (DisposablesPerTest.TryGetValue(testId, out var list)) return list;
+            throw new InvalidOperationException("Test context has no list of disposables. Missing [CleanUp]?");
+        }
+
+        public static void End(string testId)
+        {
+            if (DisposablesPerTest.TryRemove(testId, out var disposables)) disposables.Clean();
+        }
+
+        private readonly Stack<IDisposable> disposables = new Stack<IDisposable>();
+
+        public void Add(IDisposable disposable)
+        {
+            disposables.Push(disposable);
+        }
+
+        public void Clean()
+        {
+            var cleanList = disposables.ToArray();
+            disposables.Clear();
+            foreach (var item in cleanList) item.Dispose();
+        }
+    }
+}
