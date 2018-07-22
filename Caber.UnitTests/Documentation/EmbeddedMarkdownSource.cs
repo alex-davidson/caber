0ad2a92514ad3c7c291523aed4f385b78c0e1cbb
Alex Davidson <alex.davidson@bluewire-technologies.com>
2018-07-22 17:19:07 +0100

    Validate configuration examples in documentation

---
+++ Caber.UnitTests/Documentation/EmbeddedMarkdownSource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Caber.UnitTests.Documentation
+{
+    public class EmbeddedMarkdownSource
+    {
+        public IEnumerable<EmbeddedMarkdown> GetAll(Type marker = null)
+        {
+            marker = marker ?? GetType();
+            var resourceNamespace = marker.Namespace ?? throw new ArgumentException($"Cannot determine namespace to search from type {marker}");
+            return GetFrom(marker.Assembly, resourceNamespace);
+        }
+
+        private static IEnumerable<EmbeddedMarkdown> GetFrom(Assembly assembly, string resourceNamespace)
+        {
+            foreach (var resourceName in assembly.GetManifestResourceNames())
+            {
+                if (!resourceName.StartsWith(resourceNamespace + ".")) continue;
+                var fileName = resourceName.Substring(resourceNamespace.Length + 1);
+                if (Path.GetExtension(fileName) != ".md") continue;
+                yield return new EmbeddedMarkdown(assembly, resourceNamespace, fileName);
+            }
+        }
+    }
+}
---
+++ Caber.UnitTests/Documentation/XmlBlock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Caber.Configuration;
+
+namespace Caber.UnitTests.Documentation
+{
+    public class XmlBlock
+    {
+        public ConfigurationLocation Location { get; }
+        public string OriginalText { get; }
+
+        public XmlBlock(string originalText, ConfigurationLocation location)
+        {
+            OriginalText = originalText;
+            Location = location;
+        }
+
+        public IList<XElement> GetElements() => XElement.Parse($"<xml>{OriginalText}</xml>").Elements().ToList();
+
+        public override string ToString() => Location.ToString();
+    }
+}
