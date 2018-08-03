8a3e076478741eb8b3d5f9de1dcfeb571c6302a0
Alex Davidson <alex.davidson@bluewire-technologies.com>
2018-08-03 19:14:24 +0100

    wip-router

---
+++ Caber/Routing/FileChangeEventRouter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Caber.FileSystem;
+using Caber.Logging;
+using Caber.Retry;
+using Caber.Util;
+
+namespace Caber.Routing
+{
+    /// <summary>
+    /// Processes file change events.
+    /// </summary>
+    public class FileChangeEventRouter : IWorkerLoop
+    {
+        private readonly FileChangeEventQueue queue;
+        private readonly IFileSnapshotRoute[] senders;
+
+        public IClock Clock { get; set; } = Util.Clock.Default;
+        public IFileSnapshotService FileSnapshotService { get; set; }
+
+        public FileChangeEventRouter(FileChangeEventQueue queue, IFileSnapshotService fileSnapshotService, IFileSnapshotRoute[] senders)
+        {
+            this.queue = queue;
+            FileSnapshotService = fileSnapshotService;
+            this.senders = senders.ToArray();
+        }
+
+        public Task WaitForWork(CancellationToken token) => queue.Queued.WaitOneAsync(token);
+
+        public async Task RunOnce(CancellationToken token)
+        {
+            while (queue.TryDequeue(out var fileChangeEvent))
+            {
+                try
+                {
+                    var group = new RetryGroup();
+                    var needed = senders.Where(s => s.Accepts(fileChangeEvent.AbstractPath)).ToArray();
+                    if (!needed.Any()) continue;
+
+                    var snapshot = await FileSnapshotService.GetSnapshot(fileChangeEvent.QualifiedPath, Clock.Now);
+                    if (token.IsCancellationRequested) break;
+
+                    await Task.WhenAll(needed.Select(s => s.HandleAsync(snapshot, fileChangeEvent.AbstractPath, group.Parallel(), token)).ToArray());
+                    if (group.RetryRequested)
+                    {
+                        queue.Enqueue(fileChangeEvent, group.GetToken());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Operations.Error(new UnexpectedExceptionEvent(LogEventCategory.Routing, ex, fileChangeEvent.QualifiedPath.ToString()));
+                }
+                if (token.IsCancellationRequested) break;
+            }
+        }
+    }
+}
---
+++ Caber/Routing/IFileSnapshotRoute.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Caber.FileSystem;
+using Caber.Retry;
+
+namespace Caber.Routing
+{
+    public interface IFileSnapshotRoute
+    {
+        /// <summary>
+        /// Indicates whether the specified abstract path is replicated via this route.
+        /// </summary>
+        bool Accepts(AbstractPath abstractPath);
+        /// <summary>
+        /// Indicates whether the route has met its replication target for the specified snapshot as the specified abstract path.
+        /// </summary>
+        /// <remarks>
+        /// If the route has multiple senders, this should return 'true' if it has met its replication target.
+        /// </remarks>
+        bool IsHandled(FileSnapshot snapshot, AbstractPath abstractPath);
+        /// <summary>
+        /// Replicates the specified snapshot as the specified abstract path. Returns true if the destination is up to date.
+        /// </summary>
+        /// <remarks>
+        /// If the route has multiple senders it should attempt to send to all of them.
+        /// </remarks>
+        Task<RetryToken> HandleAsync(FileSnapshot snapshot, AbstractPath abstractPath, IRetryCollector retryCollector = null, CancellationToken token = default);
+    }
+}
